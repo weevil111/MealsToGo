d0609319bc24d4b5debcab8c96185361692ab663
Abhinav Mishra <abhinav.mishra@cilalabs.com>
2022-04-27 19:03:34 +0530

    Downgrade eslint to below version 8 and fix liniting errors

--- App.js
+++ App.js
@@ -5,11 +5,11 @@ import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View style={{ padding: 16, backgroundColor: "green" }}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
           <Text>Search</Text>
         </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+        <View style={styles.list}>
           <Text>List</Text>
         </View>
       </SafeAreaView>
@@ -18,4 +18,8 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1, marginTop: StatusBar.currentHeight },
+  search: { padding: 16, backgroundColor: "green" },
+  list: { flex: 1, padding: 16, backgroundColor: "blue" },
+});
