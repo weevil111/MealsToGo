4b1767db870766cc7d20afed56a27f17feaa8fef
Abhinav Mishra <abhinav.mishra@cilalabs.com>
2022-04-29 13:35:26 +0530

    Bug fix: Both closed temporarily and open was being shown for some restaurants

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -23,9 +23,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
-    isOpenNow = true,
+    isOpenNow,
     rating = 4,
-    isClosedTemporarily = true,
+    isClosedTemporarily,
   } = restaurant;
   const ratingArray = [...Array(Math.floor(rating))];
 
