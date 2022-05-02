dc9d9d33acb16be321eaee994f05cfa7f354b5db
Abhinav Mishra <abhinav.mishra@cilalabs.com>
2022-05-02 16:20:39 +0530

    Use custom callout in map section

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from "styled-components/native";
+import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.component";
 
 const MyText = styled.Text``;
 
 export const MapCallout = ({ restaurant }) => {
-  return <MyText>{restaurant.name}</MyText>;
+  return <CompactRestaurantInfo restaurant={restaurant} />;
 };
