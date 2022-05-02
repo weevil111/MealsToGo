6d45b7fa0947d42cb754c2fa9c6bfbb1ca67c530
Abhinav Mishra <abhinav.mishra@cilalabs.com>
2022-05-02 16:23:25 +0530

    Link callout on map to the restaurant details screen

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -12,7 +12,7 @@ const Map = styled(MapView)`
   width: 100%;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
   const { location } = useContext(LocationContext);
   const { restaurants = [] } = useContext(RestaurantsContext);
 
@@ -48,7 +48,11 @@ export const MapScreen = () => {
                 longitude: restaurant.geometry.location.lng,
               }}
             >
-              <MapView.Callout>
+              <MapView.Callout
+                onPress={() =>
+                  navigation.navigate("RestaurantDetail", { restaurant })
+                }
+              >
                 <MapCallout restaurant={restaurant}>Hello</MapCallout>
               </MapView.Callout>
             </MapView.Marker>
