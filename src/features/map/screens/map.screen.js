99bb5add1c2b2071b6bac26d105108026b0fd164
Abhinav Mishra <abhinav.mishra@cilalabs.com>
2022-05-02 12:33:42 +0530

    Add functionality to zoom in to selected location

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -3,6 +3,8 @@ import MapView from "react-native-maps";
 import styled from "styled-components/native";
 
 import { Search } from "../components/search.component";
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
 const Map = styled(MapView)`
   height: 100%;
@@ -10,10 +12,35 @@ const Map = styled(MapView)`
 `;
 
 export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
   return (
     <>
       <Search />
-      <Map />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return null;
+        })}
+      </Map>
     </>
   );
 };
