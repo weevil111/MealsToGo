8f3538575697599316e23331ea04a6abac151523
Abhinav Mishra <abhinav.mishra@cilalabs.com>
2022-05-03 21:38:52 +0530

    Add back button in login screen

--- src/features/account/screen/account.screen.js
+++ src/features/account/screen/account.screen.js
@@ -1,16 +1,19 @@
 import React from "react";
+
+import { Spacer } from "../../../components/spacer/spacer.component";
 import {
   AccountBackground,
   AccountContainer,
   AccountCover,
   AuthButton,
+  Title,
 } from "../components/account.styles";
-import { Spacer } from "../../../components/spacer/spacer.component";
 
 export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <Title>Meals To Go</Title>
       <AccountContainer>
         <AuthButton
           icon="lock-open-outline"
@@ -19,14 +22,15 @@ export const AccountScreen = ({ navigation }) => {
         >
           Login
         </AuthButton>
-        <Spacer size="large" />
-        <AuthButton
-          icon="email"
-          mode="contained"
-          onPress={() => navigation.navigate("Register")}
-        >
-          Register
-        </AuthButton>
+        <Spacer size="large">
+          <AuthButton
+            icon="email"
+            mode="contained"
+            onPress={() => navigation.navigate("Register")}
+          >
+            Register
+          </AuthButton>
+        </Spacer>
       </AccountContainer>
     </AccountBackground>
   );
