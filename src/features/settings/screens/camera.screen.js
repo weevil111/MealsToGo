1d47389d59896b8b37a7870c7d9037074d7bb5af
Abhinav Mishra <abhinav.mishra@cilalabs.com>
2022-05-04 19:06:18 +0530

    Make the camera functional and set profile pic on snap

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,21 +1,42 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useState, useEffect, useContext } from "react";
 import { Camera } from "expo-camera";
 import styled from "styled-components/native";
-import { View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { View, TouchableOpacity } from "react-native";
 import { Text } from "../../../components/typography/text.component";
+
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 const ProfileCamera = styled(Camera)`
   width: 100%;
   height: 100%;
 `;
-export const CameraScreen = () => {
+
+const InnerSnap = styled.View`
+  width: 100%;
+  height: 100%;
+  z-index: 999;
+`;
+
+export const CameraScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
   const cameraRef = useRef();
+  const { user } = useContext(AuthenticationContext);
+
+  const snap = async () => {
+    console.log("Hello");
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
+      navigation.goBack();
+    }
+  };
+
   useEffect(() => {
     (async () => {
-      const resp = await Camera.requestCameraPermissionsAsync();
-      console.log(resp);
-      setHasPermission(resp.status === "granted");
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
     })();
   }, []);
 
@@ -25,11 +46,14 @@ export const CameraScreen = () => {
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
-
   return (
     <ProfileCamera
       ref={(camera) => (cameraRef.current = camera)}
       type={Camera.Constants.Type.front}
-    ></ProfileCamera>
+    >
+      <TouchableOpacity onPress={snap}>
+        <InnerSnap />
+      </TouchableOpacity>
+    </ProfileCamera>
   );
 };
