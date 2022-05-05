6c684c1833a4f064616b7d7d017b902d00f28505
Abhinav Mishra <abhinav.mishra@cilalabs.com>
2022-05-05 18:49:49 +0530

    Use places API for fetching data instead of mock

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,14 @@ const functions = require("firebase-functions");
 const { geocodeRequest } = require("./geocode");
 const { placesRequest } = require("./places");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const { Client } = require("@googlemaps/google-maps-services-js");
+
+const client = new Client({});
+
 exports.geocode = functions.https.onRequest((request, response) => {
-  geocodeRequest(request, response);
+  geocodeRequest(request, response, client);
 });
 
 exports.placesNearby = functions.https.onRequest((request, response) => {
-  placesRequest(request, response);
+  placesRequest(request, response, client);
 });
