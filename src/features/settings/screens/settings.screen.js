02970daead644a8f21ac87a3f55ffd8122da20de
Abhinav Mishra <abhinav.mishra@cilalabs.com>
2022-05-10 20:10:03 +0530

    Improve the design of settings screen

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -12,8 +12,20 @@ import { SafeArea } from "../../../components/utility/safe-area.component";
 import { colors } from "../../../infrastructure/theme/colors";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
+const TransparentSafeArea = styled(SafeArea)`
+  background-color: transparent;
+`;
+const SettingsBackground = styled.ImageBackground.attrs({
+  source: require("../../../../assets/home_bg.jpg"),
+})`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+`;
+
 const SettingsItem = styled(List.Item)`
   padding: ${(props) => props.theme.space[3]};
+  background-color: rgba(255, 255, 255, 0.4);
 `;
 const AvatarContainer = styled.View`
   align-items: center;
@@ -35,42 +47,69 @@ export const SettingsScreen = ({ navigation }) => {
   );
 
   return (
-    <SafeArea>
-      <AvatarContainer>
-        <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
-          {!photo && (
-            <Avatar.Icon
-              size={180}
-              icon="human"
-              backgroundColor={colors.brand.primary}
-            />
-          )}
-          {photo && (
-            <Avatar.Image
-              size={180}
-              source={{ uri: photo }}
-              backgroundColor="#2182BD"
-            />
-          )}
-        </TouchableOpacity>
-        <Spacer position="top" size="large">
-          <Text variant="label">{user.email}</Text>
-        </Spacer>
-      </AvatarContainer>
+    <SettingsBackground>
+      <TransparentSafeArea>
+        <AvatarContainer>
+          <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
+            {!photo && (
+              <Avatar.Icon
+                size={180}
+                icon="human"
+                backgroundColor={colors.brand.primary}
+              />
+            )}
+            {photo && (
+              <Avatar.Image
+                size={180}
+                source={{ uri: photo }}
+                backgroundColor="#2182BD"
+              />
+            )}
+          </TouchableOpacity>
+          <Spacer position="top" size="large">
+            <Text variant="label">{user.email}</Text>
+          </Spacer>
+        </AvatarContainer>
 
-      <List.Section>
-        <SettingsItem
-          title="Favourites"
-          description="View your favourites"
-          left={(props) => <List.Icon {...props} color="black" icon="heart" />}
-          onPress={() => navigation.navigate("Favourites")}
-        />
-        <SettingsItem
-          title="Logout"
-          left={(props) => <List.Icon {...props} color="black" icon="door" />}
-          onPress={onLogout}
-        />
-      </List.Section>
-    </SafeArea>
+        <List.Section>
+          <SettingsItem
+            title="Favourites"
+            description="View your favourites"
+            left={(props) => (
+              <List.Icon {...props} color={colors.ui.error} icon="heart" />
+            )}
+            onPress={() => navigation.navigate("Favourites")}
+          />
+          <Spacer />
+          <SettingsItem
+            title="Payment"
+            left={(props) => (
+              <List.Icon {...props} color={colors.ui.secondary} icon="cart" />
+            )}
+            onPress={() => null}
+          />
+          <Spacer />
+          <SettingsItem
+            title="Past Orders"
+            left={(props) => (
+              <List.Icon
+                {...props}
+                color={colors.ui.secondary}
+                icon="history"
+              />
+            )}
+            onPress={() => null}
+          />
+          <Spacer />
+          <SettingsItem
+            title="Logout"
+            left={(props) => (
+              <List.Icon {...props} color={colors.ui.secondary} icon="door" />
+            )}
+            onPress={onLogout}
+          />
+        </List.Section>
+      </TransparentSafeArea>
+    </SettingsBackground>
   );
 };
